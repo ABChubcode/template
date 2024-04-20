3031d7db4e3936d6a99f4390893edd13109b5c32
Africa's Blockchain Club <166007706+ABChubcode@users.noreply.github.com>
2024-04-20 09:59:30 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import { TransactionsProvider } from "./context/TransactionContext";
 import "./index.css";
 
 ReactDOM.render(
