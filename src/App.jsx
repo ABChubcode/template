09da7384c2f0e199785156a04ec98d29138e33c4
Africa's Blockchain Club <166007706+ABChubcode@users.noreply.github.com>
2024-04-20 11:49:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@
 
 const App = () => {
   return (
-    <div className="min-h-screen">
-      
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
