025cfb3871f978caa81fade7237f4eedd823af90
Africa's Blockchain Club <166007706+ABChubcode@users.noreply.github.com>
2024-04-20 09:58:53 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { Navbar, Welcome, Footer, Services } from './components';
+
 
 const App = () => {
   return (
     <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-        <Navbar />
-        <Welcome />
-      </div>
-      <Services />
       
-      <Footer />
     </div>
   );
 }
